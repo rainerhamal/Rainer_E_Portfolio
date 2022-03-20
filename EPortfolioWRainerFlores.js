5745b88a23f1201e96bb2a9cfa74b32543662265
Rainer Flores <rainerneno@gmail.com>
2022-03-20 11:40:58 +0800

    updated

--- EPortfolioWRainerFlores.js
+++ EPortfolioWRainerFlores.js
@@ -104,3 +104,167 @@ cvDownload.addEventListener('click', () => {
         cvDownload.style.transition = "1s ease-in-out";
     }, 3000)
 })
+
+
+//Fade-in Section
+
+const faders = document.querySelectorAll(".fade-in");
+
+const appearOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px -200px 0px"
+};
+const appearOnScroll = new IntersectionObserver(function(
+        entries,
+        appearOnScroll) {
+            entries.forEach(entry => {
+                if (!entry.isIntersecting) {
+                    return;
+                } else {
+                    entry.target.classList.add('appear');
+                    appearOnScroll.unobserve(entry.target)
+                }
+            })
+        }, 
+        appearOptions);
+
+        faders.forEach(fader => {
+            appearOnScroll.observe(fader);
+        })
+
+
+//Contact Us Validation
+let contactForm = document.querySelector("#contactForm");
+let fName = document.querySelector("#fName");
+let LName = document.querySelector("#LName");
+let email = document.querySelector("#email");
+let contactNumber = document.querySelector("#contactNumber");
+let message = document.querySelector("#message");
+let submit = document.querySelector("#submit");
+
+contactForm.addEventListener('input', (event2) => {
+    let targetVar = event2.target.id;
+    // console.log(event2.target.id);
+    switch(targetVar) {
+        case 'fName':
+            checkfName();
+            break;
+        case 'LName':
+            checkLName();
+            break;
+        case 'email':
+            checkEmail();
+            break;
+        case 'contactNumber':
+            checkNumber();
+            break;
+        case 'message':
+            checkMessage();
+            break;
+    }
+});
+
+
+contactForm.addEventListener('submit', (event) => {
+    checkfName();
+    checkLName()
+    checkEmail();
+    checkNumber();
+    checkMessage();
+    event.preventDefault();
+});
+
+function checkfName() {
+    let fnameValue = fName.value;
+    //name validation
+    if (fnameValue === "") {
+        document.querySelector("#error1").innerText = "Enter your First Name";
+    } else {
+        let fnameFinal = fnameValue.trim();
+        if (fnameFinal.length < 3) {
+            document.querySelector("#error1").innerText = "Enter your First Name";
+        } else {
+            document.querySelector("#error1").innerText = `${fnameFinal}`;
+        }
+    }
+
+};
+
+function checkLName() {
+    let LnameValue = LName.value;
+    //name validation
+    if (LnameValue === "") {
+        document.querySelector("#error2").innerText = "Enter your Last Name";
+    } else {
+        let LnameFinal = LnameValue.trim();
+        if (LnameFinal.length < 5) {
+            document.querySelector("#error2").innerText = "Enter your Last Name";
+        } else {
+            document.querySelector("#error2").innerText = `${LnameFinal}`;
+        }
+    }
+
+};
+
+function checkEmail() {
+    let emailValue = email.value;
+
+    if (emailValue === "") {
+        document.querySelector("error2").innertext = "Email must be filled out!";
+    } else {
+        let regEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        let emailFinal = emailValue.trim();
+        if ((regEmail.test(emailFinal)) && (emailFinal.indexOf(" ") === -1)) {
+            document.querySelector("#error3").innerText = "Valid Email";
+        } else {
+            document.querySelector("#error3").innerText = "Email Address must be username@email.com and must not have any spaces!";
+        }
+    }
+};
+
+
+function checkNumber() {
+    let contactNumberValue = contactNumber.value;
+    let contactNumberFinal = contactNumberValue.trim();
+
+        let phoneNum = /^\+?([0-9]{2})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{3})?[-. ]?([0-9]{4})$/;
+        if ((phoneNum.test(contactNumberFinal))){
+            document.querySelector("#error4").innerText = "Valid Number";
+        } else {
+            document.querySelector("#error4").innerText = "Phone number must be in the following format +XX-XXX-XXX-XXXX, +XX.XXX.XXX.XXXX, +XX XXX XXX XXXX";
+        }
+};
+
+function checkMessage() {
+    let messageValue = message.value
+    let messageFinal = messageValue.trim();
+        if (messageFinal.length < 50) {
+            document.querySelector("#error5").innerText = "Please compose a detailed message!";
+        } else {
+            document.querySelector("#error5").innerText = "Thank you for your Inquiry!";
+        }
+}
+
+let contactInquiryArray = [];
+let addInquiry = document.querySelector("#submit");
+
+let addContactInquiry = (e) => {
+    let contactInquiry ={
+        id: Date.now(),
+        fname: document.querySelector("#fName").value,
+        Lname: document.querySelector("#LName").value,
+        email: document.querySelector("#email").value,
+        number: document.querySelector("#contactNumber").value,
+        message: document.querySelector("#message").value,
+    }
+
+    contactInquiryArray.push(contactInquiry);
+    document.querySelector("#contactForm").reset();
+    console.table(contactInquiryArray);
+
+    localStorage.setItem("Contact Inquiries", JSON.stringify(contactInquiryArray));
+
+    e.preventDefault();
+}
+
+addInquiry.addEventListener('click', addContactInquiry);
